b92e715435d136ebd6d67e1a8c18881df2e4420c
SUKHI <86770159+SUKHPAL443@users.noreply.github.com>
2021-10-02 11:01:00 +0530

    Update events.py

--- ULTRA/events.py
+++ ULTRA/events.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 """ Userbot module for managing events.
- One of the main components of the ULTRA. """
+ One of the main components of the BLACK_MAMBA. """
 
 import sys
 from asyncio import create_subprocess_shell as asyncsubshell
