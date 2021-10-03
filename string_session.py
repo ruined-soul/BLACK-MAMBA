007b015c877a4c7668c98a9a530ed8d1d6b181b5
SUKHI <86770159+SUKHPAL443@users.noreply.github.com>
2021-10-03 14:49:37 +0530

    Create string_session.py

---
+++ string_session.py
@@ -0,0 +1,56 @@
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
+import random
+from colorama import Fore, Style, Back
+
+
+BLACK_MAMBA = """
+╔╗──╔═══╦═══╦═══╦═╗─╔╦═══╦═══╦═══╦╗──╔╗
+║║──║╔══╣╔═╗║╔══╣║╚╗║╠╗╔╗║╔═╗║╔═╗║╚╗╔╝║  
+║║──║╚══╣║─╚╣╚══╣╔╗╚╝║║║║║║─║║╚═╝╠╗╚╝╔╝  
+║║─╔╣╔══╣║╔═╣╔══╣║╚╗║║║║║║╚═╝║╔╗╔╝╚╗╔╝   
+║╚═╝║╚══╣╚╩═║╚══╣║─║║╠╝╚╝║╔═╗║║║╚╗─║║   
+╚═══╩═══╩═══╩═══╩╝─╚═╩═══╩╝─╚╩╝╚═╝─╚╝   
+"""
+logo = """
+"""
+bhai_bolte = """
+#USERBOT NAME = BLACK_MAMBA          
+Made With Love By Team BLACK_MAMBA
+"""
+                                                                                                            
+print("")
+print(Style.BRIGHT + Fore.MAGENTA + Legend)
+print(Style.RESET_ALL)
+print(Style.BRIGHT + Fore.BLUE + logo)
+print(Style.RESET_ALL)
+print(Style.BRIGHT + Fore.CYAN + Back.BLUE + bhai_bolte)
+print(Style.RESET_ALL)
+print("""Welcome To BLACK_MAMBA String Generator By @BLACK_MAMBA_SUPPORT""")
+print("""Kindly Enter Your Details To Continue ! """)
+
+API_KEY = input("API_KEY: ")
+API_HASH = input("API_HASH: ")
+
+while True:
+    try:
+        with TelegramClient(StringSession(), API_KEY, API_HASH) as client:
+            print("String Sent To Your Saved Message, Store It To A Safe Place!! ")
+            print("")
+            session = client.session.save()
+            client.send_message(
+                "me",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @BLACK_MAMBA_SUPPORT For Any Help !",
+            )
+
+            print(
+                "Thanks for Choosing BLACK_MAMBA Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+            )
+    except:
+        print("")
+        print(
+            "Wrong phone number \n make sure its with correct country code. Example : +919811099999 ! Kindly Retry"
+        )
+        print("")
+        continue
+    break
